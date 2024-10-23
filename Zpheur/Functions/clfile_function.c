a27ad1e77da481e96889153c6a643df66aeace4c
Sultan Ilham <onecthree@gmail.com>
2024-10-23 21:52:08 +0700

    update fix

--- Zpheur/Functions/clfile_function.c
+++ Zpheur/Functions/clfile_function.c
@@ -14,29 +14,29 @@ PHP_FUNCTION(clfile)
 	ZEND_PARSE_PARAMETERS_START(1, 1)
 		Z_PARAM_STRING(class_name_src, class_name_len)
 	ZEND_PARSE_PARAMETERS_END();
-
-	/**
-	 * Attention: Z_PARAM_STRING return a ref variable instead a copy
-	 * if you do manipulated string, it will changed to PHP-land.
-	 * Make a copy instead
-	 */
-	unsigned char _copy_class_name[class_name_len + 1];
-
+	
 	if( class_name_len && !isalpha(class_name_src[0]) )
 		php_error_docref(
 			NULL, E_ERROR, 
 			"the identifier for the class name cannot begin with a digit");
 
-	_copy_class_name[0] = tolower(class_name_src[0]);
-	_copy_class_name[class_name_len] = '\0';
+	/**
+	 * Attention: Z_PARAM_STRING return a ref of zval_string struct
+	 * instead a copy if you do manipulated string, it will changed to PHP-land.
+	 * Make a copy instead
+	 */
+	unsigned char _class_name_src[class_name_len + 1];
+
+	_class_name_src[0] = tolower(class_name_src[0]);
+	_class_name_src[class_name_len] = '\0';
 
 	for( size_t i = 1; i < class_name_len; ++i )
 	{
 		if( class_name_src[i] == '\\' )
-			_copy_class_name[i] = '/';
+			_class_name_src[i] = '/';
 		else
-			_copy_class_name[i] = class_name_src[i];
+			_class_name_src[i] = class_name_src[i];
 	}
 
-	RETURN_STRINGL(_copy_class_name, class_name_len);
+	RETURN_STRINGL(_class_name_src, class_name_len);
 }
