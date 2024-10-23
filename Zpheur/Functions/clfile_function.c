8a159c8123e069f5de470428df3ba406a1fc3b16
Sultan Ilham <onecthree@gmail.com>
2024-10-23 21:03:58 +0700

    fix forget copy problem, previous fix

--- Zpheur/Functions/clfile_function.c
+++ Zpheur/Functions/clfile_function.c
@@ -15,15 +15,28 @@ PHP_FUNCTION(clfile)
 		Z_PARAM_STRING(class_name_src, class_name_len)
 	ZEND_PARSE_PARAMETERS_END();
 
+	/**
+	 * Attention: Z_PARAM_STRING return a ref variable instead a copy
+	 * if you do manipulated string, it will changed to PHP-land.
+	 * Make a copy instead
+	 */
+	unsigned char _copy_class_name[class_name_len + 1];
+
 	if( class_name_len && !isalpha(class_name_src[0]) )
 		php_error_docref(
 			NULL, E_ERROR, 
 			"the identifier for the class name cannot begin with a digit");
 
-	class_name_src[0] = tolower(class_name_src[0]);
-	for( int i = 1; i < class_name_len; ++i )
-		if( class_name_src[i] == '\\' )
-			class_name_src[i] = '/';
+	_copy_class_name[0] = tolower(class_name_src[0]);
+	_copy_class_name[class_name_len] = '\0';
 
-	RETURN_STRINGL(class_name_src, class_name_len);
+	for( size_t i = 1; i < class_name_len; ++i )
+	{
+		if( class_name_src[i] == '\\' )
+			_copy_class_name[i] = '/';
+		else
+			_copy_class_name[i] = class_name_src[i];
+	}
+
+	RETURN_STRINGL(_copy_class_name, class_name_len);
 }
