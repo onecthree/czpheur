a1738894a49eff76864dab46815f041b8f686792
Sultan Ilham <onecthree@gmail.com>
2024-11-05 02:02:17 +0700

    fix/improve function call, format, minor update

--- include/runtime.c
+++ include/runtime.c
@@ -1,25 +1,57 @@
-#include "php.h"
-#include "php_main.h"
-#include "ext/standard/info.h"
-#include "php_zpheur.h"
+
+#include <php.h>
+#include <php_main.h>
+#include <ext/standard/info.h>
+#include <php_zpheur.h>
 #include <zpheur.h>
 #include "runtime.h"
 
-#include "ext/spl/php_spl.h"
-#include "ext/spl/spl_functions.h"
-// #include "ext/spl/spl_engine.h"
-#include "ext/spl/spl_array.h"
-#include "ext/spl/spl_directory.h"
-#include "ext/spl/spl_iterators.h"
-#include "ext/spl/spl_exceptions.h"
-#include "ext/spl/spl_observer.h"
-#include "ext/spl/spl_dllist.h"
-#include "ext/spl/spl_fixedarray.h"
-#include "ext/spl/spl_heap.h"
-#include "zend_exceptions.h"
-#include "zend_interfaces.h"
+#include <ext/standard/basic_functions.h>
+#include <ext/spl/php_spl.h>
+#include <ext/spl/spl_functions.h>
 
-zval *php_call_enum_method( zend_class_entry *enum_ce, const char *function_name, unsigned int param_counts, zval *params )
+#if PHP_VERSION_ID < 80400 // spl_engine.h removed in PHP ^8.4.x
+#include <ext/spl/spl_engine.h>
+#endif
+
+#include <ext/spl/spl_array.h>
+#include <ext/spl/spl_directory.h>
+#include <ext/spl/spl_iterators.h>
+#include <ext/spl/spl_exceptions.h>
+#include <ext/spl/spl_observer.h>
+#include <ext/spl/spl_dllist.h>
+#include <ext/spl/spl_fixedarray.h>
+#include <ext/spl/spl_heap.h>
+#include <zend_exceptions.h>
+#include <zend_interfaces.h>
+
+
+#include <zend.h>
+#include <zend_compile.h>
+#include <zend_execute.h>
+#include <zend_API.h>
+#include <zend_stack.h>
+#include <zend_constants.h>
+#include <zend_extensions.h>
+#include <zend_exceptions.h>
+#include <zend_closures.h>
+#include <zend_generators.h>
+#include <zend_vm.h>
+#include <zend_float.h>
+#include <zend_fibers.h>
+#include <zend_weakrefs.h>
+#include <zend_inheritance.h>
+#include <zend_observer.h>
+
+#include <Zend/zend_exceptions.h>
+
+
+zval* php_call_enum_method(
+    zend_class_entry*   enum_ce,
+    char* const         function_name,
+    unsigned int        param_counts,
+    zval*               params
+)
 {
     zval fun_str;
     ZVAL_STRING(&fun_str, function_name);
@@ -61,172 +93,118 @@ zval *php_call_enum_method( zend_class_entry *enum_ce, const char *function_name
     return NULL; // Error calling the function
 }
 
-zval *php_call_function( const char *function_name, unsigned int param_counts, zval *params )
+zend_result php_call_function(
+    char* const function_name_src, 
+    size_t function_name_len,
+    zend_long param_counts,
+    HashTable* named_params,
+    zval* params,
+    zval* retval
+)
 {
-    zval fun_str;
-    ZVAL_STRING(&fun_str, function_name);
     zend_fcall_info fci;
-    zend_fcall_info_cache fci_cache;
+    zend_fcall_info_cache fcc;
+    zval function_name;
 
-    zend_string* function_name_str = zend_string_init(function_name, strlen(function_name), 0);
-    zend_function* func = zend_hash_find_ptr(EG(function_table), function_name_str);
+    ZVAL_STRINGL(&function_name, function_name_src, function_name_len);
 
-    if (func == NULL) {
-        zend_string_release(function_name_str);
-        return NULL; // Function not found
-    }
-
-    zval *retval = emalloc(sizeof(zval));
+    zend_function* function_ptr =
+        zend_hash_str_find_ptr(
+            EG(function_table), "main", sizeof("main") - 1
+        );
 
     fci.size = sizeof(fci);
-    fci.function_name = fun_str;
-    fci.retval = &*retval;
-    fci.param_count = param_counts;
+    fci.function_name = function_name;
+    fci.retval = retval;
     fci.params = params;
     fci.object = NULL;
-    // fci.no_separation = 0;
-    fci.named_params = NULL;
+    fci.param_count = param_counts;
+    fci.named_params = named_params;
 
-    // fci_cache.initialized = 1;
-    fci_cache.called_scope = NULL;
-    fci_cache.calling_scope = NULL;
-    fci_cache.function_handler = func;
+    fcc.called_scope = NULL;
+    fcc.calling_scope = NULL;
+    fcc.object = NULL;
+    fcc.function_handler = function_ptr;
 
-    // zend_call_function(&fci, &fci_cache);
+    if( zend_call_function(&fci, &fcc) != SUCCESS )
+        return FAILURE;
 
-    if ( zend_call_function(&fci, &fci_cache) == SUCCESS )
-    {
-        zend_string_release(function_name_str);
-        return retval;
-    }
-    return NULL; // Error calling the function
+    return SUCCESS;
 }
 
-zval *php_call_static_method_2( zend_object *obj, const char *function_name, unsigned int param_counts, zval *params )
+/**
+ * experimental
+ * {{
+int php_execute_file( char* const file_path, int flag_mode )
 {
-    zval fun_str;
-    ZVAL_STRING(&fun_str, function_name);
-    zend_fcall_info fci;
-    zend_fcall_info_cache fci_cache;
+   zend_string *zs_file_path = zend_string_init(file_path, strlen(file_path), 0);
+   zval dummy;
+   zend_file_handle file_handle;
+   zend_op_array *new_op_array;
+   zval result;
+   int ret;
 
-    zend_string* function_name_str = zend_string_init(function_name, strlen(function_name), 0);
-    zend_function* func;
+//    zend_stream_init_filename_ex(&file_handle, class_file);
+//    ret = php_stream_open_for_zend_ex(&file_handle, USE_PATH|STREAM_OPEN_FOR_INCLUDE);
+   file_handle.type = ZEND_HANDLE_FILENAME;
+   file_handle.filename = zs_file_path;
 
-    ZEND_HASH_FOREACH_PTR(&obj->ce->function_table, zend_function *funcs)
-    {
-        if (funcs->common.fn_flags & ZEND_ACC_PUBLIC) {
-            if( strcmp(funcs->common.function_name->val, function_name) == 0 )
-            {
-                func = funcs;
-            }
-        }
-    }
-    ZEND_HASH_FOREACH_END();
+   zend_string *opened_path;
+   if (!file_handle.opened_path) {
+       file_handle.opened_path = zend_string_copy(zs_file_path);
+   }
 
-    // return NULL;
+   opened_path = zend_string_copy(file_handle.opened_path);
 
-    if (func == NULL) {
-        zend_string_release(function_name_str);
-        php_printf("ga ada");
-        return NULL; // Function not found
-    }
+   ZVAL_NULL(&dummy);
+   if (zend_hash_add(&EG(included_files), opened_path, &dummy)) {
+       switch( flag_mode )
+       {
+           case REQUIRE:
+               new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE);
+           break;
+           case REQUIRE_ONCE:
+               new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE_ONCE);
+           break;
+           case INCLUDE:
+               new_op_array = zend_compile_file(&file_handle, ZEND_INCLUDE);
+           break;
+           case INCLUDE_ONCE:
+               new_op_array = zend_compile_file(&file_handle, ZEND_INCLUDE_ONCE);
+           break;
+       }
+   } else {
+       new_op_array = NULL;
+   }
 
-    zval *retval = emalloc(sizeof(zval));
+   zend_string_release_ex(opened_path, 0);
+   if (new_op_array) {
+       uint32_t orig_jit_trace_num = EG(jit_trace_num);
 
-    fci.size = sizeof(fci);
-    fci.function_name = fun_str;
-    // fci.retval = &(*retval);
-    fci.param_count = param_counts;
-    fci.params = params;
-    fci.object = NULL;
-    // fci.no_separation = 0;
-    fci.named_params = NULL;
+       ZVAL_UNDEF(&result);
+       zend_execute(new_op_array, &result);
+       EG(jit_trace_num) = orig_jit_trace_num;
 
-    // fci_cache.initialized = 1;
-    fci_cache.called_scope = NULL;
-    fci_cache.calling_scope = NULL;
-    fci_cache.function_handler = func;
-    fci_cache.object = obj;
+       destroy_op_array(new_op_array);
+       efree(new_op_array);
+       if (!EG(exception)) {
+           zval_ptr_dtor(&result);
+       }
 
-    zend_call_function(&fci, &fci_cache);
+       zend_destroy_file_handle(&file_handle);
+       zend_string_release(zs_file_path);
 
-    // if ( zend_call_function(&fci, &fci_cache) == SUCCESS )
-    // {
-    //     // zend_string_release(function_name_str);
-    //     return retval;
-    // }
-    return NULL; // Error calling the function
+       return 0;
+   }
+
+   zend_destroy_file_handle(&file_handle);
+   zend_string_release(zs_file_path);
+
+   return 0;
 }
+}} */
 
-//int php_execute_file( char const *file_path, int flag_mode )
-//{
-//    zend_string *zs_file_path = zend_string_init(file_path, strlen(file_path), 0);
-//    zval dummy;
-//    zend_file_handle file_handle;
-//    zend_op_array *new_op_array;
-//    zval result;
-//    int ret;
-//
-////    zend_stream_init_filename_ex(&file_handle, class_file);
-////    ret = php_stream_open_for_zend_ex(&file_handle, USE_PATH|STREAM_OPEN_FOR_INCLUDE);
-//    file_handle.type = ZEND_HANDLE_FILENAME;
-//    file_handle.filename = zs_file_path;
-//
-//    zend_string *opened_path;
-//    if (!file_handle.opened_path) {
-//        file_handle.opened_path = zend_string_copy(zs_file_path);
-//    }
-//
-//    opened_path = zend_string_copy(file_handle.opened_path);
-//
-//    ZVAL_NULL(&dummy);
-//    if (zend_hash_add(&EG(included_files), opened_path, &dummy)) {
-//        switch( flag_mode )
-//        {
-//            case REQUIRE:
-//                new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE);
-//            break;
-//            case REQUIRE_ONCE:
-//                new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE_ONCE);
-//            break;
-//            case INCLUDE:
-//                new_op_array = zend_compile_file(&file_handle, ZEND_INCLUDE);
-//            break;
-//            case INCLUDE_ONCE:
-//                new_op_array = zend_compile_file(&file_handle, ZEND_INCLUDE_ONCE);
-//            break;
-//        }
-//    } else {
-//        new_op_array = NULL;
-//    }
-//
-//    zend_string_release_ex(opened_path, 0);
-//    if (new_op_array) {
-//        uint32_t orig_jit_trace_num = EG(jit_trace_num);
-//
-//        ZVAL_UNDEF(&result);
-//        zend_execute(new_op_array, &result);
-//        EG(jit_trace_num) = orig_jit_trace_num;
-//
-//        destroy_op_array(new_op_array);
-//        efree(new_op_array);
-//        if (!EG(exception)) {
-//            zval_ptr_dtor(&result);
-//        }
-//
-//        zend_destroy_file_handle(&file_handle);
-//        zend_string_release(zs_file_path);
-//
-//        return 0;
-//    }
-//
-//    zend_destroy_file_handle(&file_handle);
-//    zend_string_release(zs_file_path);
-//
-//    return 0;
-//}
-
-void php_execute_file( char const *filepath, int flag_mode, zend_op_array* t_op_array )
+void php_execute_file( char* const filepath, int flag_mode, zend_op_array* t_op_array )
 {
     zend_string *file_path = zend_string_init(filepath, strlen(filepath), 0);
 
@@ -282,13 +260,13 @@ zval* php_call_closure( zval *z_closure, zend_object *o_closure, unsigned int pa
     return NULL;
 }
 
-inline static zend_class_entry *php_class_exists( zend_string *class_name )
+zend_class_entry* php_class_exists( zend_string* class_name )
 {
     zend_class_entry* class_entry;
     return zend_lookup_class(class_name);
 }
 
-zend_class_entry *php_enum_init( char const* class_name )
+zend_class_entry *php_enum_init( char* const class_name )
 {
     zend_class_entry *ce;
     zend_string *zstr_class_name;
@@ -311,14 +289,7 @@ zend_class_entry *php_enum_init( char const* class_name )
     return ce;
 }
 
-void php_call_static_method(
-        zend_object *obj,
-        const char *method_name, 
-        size_t method_name_length, 
-        int num_params, 
-        zval* params,
-        zval* retval
-    ) 
+void php_call_static_method( zend_object* obj, char* const method_name, size_t method_name_length, int num_params, zval* params, zval* retval )
 {
     zval function_name;
     ZVAL_STRINGL(&function_name, method_name, method_name_length);
@@ -333,7 +304,7 @@ void php_call_static_method(
     // zval_ptr_dtor(&function_name);
 }
 
-zend_object *php_class_init_self_ex( char const* class_name )
+zend_object *php_class_init_self_ex( char* const  class_name )
 {
     zend_class_entry *ce;
     zend_string *zstr_class_name;
@@ -356,7 +327,7 @@ zend_object *php_class_init_self_ex( char const* class_name )
     return object;
 }
 
-zend_object *php_class_init_ex( char const* class_name )
+zend_object *php_class_init_ex( char* const  class_name )
 {
     zend_class_entry *ce;
     zend_string *zstr_class_name;
@@ -379,7 +350,7 @@ zend_object *php_class_init_ex( char const* class_name )
     return object;
 }
 
-zend_object *php_class_init( char const* class_name )
+zend_object *php_class_init( char* const  class_name )
 {
     zend_class_entry *ce;
     zend_string *zstr_class_name;
@@ -409,11 +380,7 @@ zend_object *php_class_init( char const* class_name )
     return object;
 }
 
-void php_class_call_constructor(
-        zend_object*    zend_object_class, 
-        unsigned int    param_counts, 
-        zval*           params
-    )
+void php_class_call_constructor( zend_object* zend_object_class, unsigned int param_counts, zval* params )
 {
     zend_string*    method          =  zend_string_init("__construct", sizeof("__construct") - 1, 0);
     zval*           return_value    =  (zval*)emalloc(sizeof(zval));
@@ -446,14 +413,7 @@ void php_class_call_constructor(
     return_void: { }
 }
 
-zval* php_class_call_method(
-        zend_object*    zend_object_class, 
-        char*           method_src,
-        zend_ulong      method_len,
-        unsigned int    param_counts, 
-        zval*           params, 
-        bool            silent
-    )
+zval* php_class_call_method( zend_object* zend_object_class, char* const method_src, size_t method_len, unsigned int param_counts, zval* params, bool silent )
 {
     zend_string*    method          =  zend_string_init(method_src, method_len, 0);
     zval*           return_value    =  (zval*)emalloc(sizeof(zval));
--- include/runtime.h
+++ include/runtime.h
@@ -2,24 +2,29 @@
 #ifndef H_INCLUDE_RUNTIME
 #define H_INCLUDE_RUNTIME
 
-#include "php.h"
-#include "php_main.h"
-#include "ext/standard/info.h"
-#include "php_zpheur.h"
+#include <php.h>
+#include <php_main.h>
+#include <ext/standard/info.h>
+#include <php_zpheur.h>
+#include <zpheur.h>
 
-#include "ext/spl/php_spl.h"
-#include "ext/spl/spl_functions.h"
-// #include "ext/spl/spl_engine.h"
-#include "ext/spl/spl_array.h"
-#include "ext/spl/spl_directory.h"
-#include "ext/spl/spl_iterators.h"
-#include "ext/spl/spl_exceptions.h"
-#include "ext/spl/spl_observer.h"
-#include "ext/spl/spl_dllist.h"
-#include "ext/spl/spl_fixedarray.h"
-#include "ext/spl/spl_heap.h"
-#include "zend_exceptions.h"
-#include "zend_interfaces.h"
+#include <ext/spl/php_spl.h>
+#include <ext/spl/spl_functions.h>
+
+#if PHP_VERSION_ID < 80400 // spl_engine.h removed in PHP ^8.4.x
+#include <ext/spl/spl_engine.h>
+#endif
+
+#include <ext/spl/spl_array.h>
+#include <ext/spl/spl_directory.h>
+#include <ext/spl/spl_iterators.h>
+#include <ext/spl/spl_exceptions.h>
+#include <ext/spl/spl_observer.h>
+#include <ext/spl/spl_dllist.h>
+#include <ext/spl/spl_fixedarray.h>
+#include <ext/spl/spl_heap.h>
+#include <zend_exceptions.h>
+#include <zend_interfaces.h>
 
 #define REQUIRE 0
 #define REQUIRE_ONCE 1
@@ -27,49 +32,82 @@
 #define INCLUDE_ONCE 3
 
 
-zval *php_call_enum_method( zend_class_entry *enum_ce, const char *function_name, unsigned int param_counts, zval *params );
+zval* php_call_enum_method(
+    zend_class_entry*   enum_ce,
+    char* const         function_name,
+    unsigned int        param_counts,
+    zval*               params
+);
 
-zval *php_call_function( const char *function_name, unsigned int param_counts, zval *params );
+zend_result php_call_function(
+    char* const         function_name_src,
+    size_t              function_name_len,
+    zend_long           param_counts,
+    HashTable*          named_params,
+    zval*               params, 
+    zval*               retval
+);
 
-zval *php_call_static_method_2( zend_object *obj, const char *function_name, unsigned int param_counts, zval *params );
+// experimental
+void php_execute_file(
+    char* const         filepath,
+    int                 flag_mode,
+    zend_op_array*      t_op_array
+);
 
-void php_execute_file( char const *filepath, int flag_mode, zend_op_array* t_op_array );
+zval* php_call_closure(
+    zval*               z_closure,
+    zend_object*        o_closure,
+    unsigned int        param_counts,
+    zval*               params
+);
 
-zval* php_call_closure( zval *z_closure, zend_object *o_closure, unsigned int param_counts, zval *params );
-inline static zend_class_entry *php_class_exists( zend_string *class_name );
+zend_class_entry* php_class_exists(
+    zend_string*        class_name
+);
 
-zend_class_entry *php_enum_init( char const* class_name );
+zend_class_entry* php_enum_init(
+    char* const         class_name
+);
 
 void php_call_static_method(
-        zend_object *obj,
-        const char *method_name, 
-        size_t method_name_length, 
-        int num_params, 
-        zval* params,
-        zval* retval
-    );
+    zend_object*        obj,
+    char* const         method_name, 
+    size_t              method_name_length, 
+    int                 num_params, 
+    zval*               params,
+    zval*               retval
+);
 
-zend_object *php_class_init_self_ex( char const* class_name );
+zend_object* php_class_init_self_ex(
+    char* const         class_name
+);
 
-zend_object *php_class_init_ex( char const* class_name );
+zend_object* php_class_init_ex(
+    char* const         class_name
+);
 
-zend_object *php_class_init( char const* class_name );
+zend_object* php_class_init(
+    char* const         class_name
+);
 
 void php_class_call_constructor(
-        zend_object*    zend_object_class, 
-        unsigned int    param_counts, 
-        zval*           params
-    );
+    zend_object*        zend_object_class, 
+    unsigned int        param_counts, 
+    zval*               params
+);
 
 zval* php_class_call_method(
-        zend_object*    zend_object_class, 
-        char*           method_src,
-        zend_ulong      method_len,
-        unsigned int    param_counts, 
-        zval*           params, 
-        bool            silent
-    );
+    zend_object*        zend_object_class, 
+    char* const         method_src,
+    size_t              method_len,
+    unsigned int        param_counts, 
+    zval*               params, 
+    bool                silent
+);
 
-char* const ZTYPE_TO_STR( char M_TYPE );
+char* const ZTYPE_TO_STR(
+    char                M_TYPE
+);
 
 #endif
