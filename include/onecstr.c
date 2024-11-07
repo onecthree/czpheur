0a06ee68c9009d5979ee727067e8697375a5b552
Sultan Ilham <onecthree@gmail.com>
2024-11-07 19:15:39 +0700

    add new onec_stringcl struct and functions impl

--- include/onecstr.c
+++ include/onecstr.c
@@ -1,16 +1,13 @@
 
-#include <php.h>
-#include <ext/standard/info.h>
-#include <php_zpheur.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 #include "onecstr.h"
 
-#define onec_string_init( onec_str ) _onec_string_init(&onec_str)
-void _onec_string_init( onec_string** onec_str )
+
+void inline __attribute__ ((always_inline))
+_onec_string_init( onec_string** onec_str )
 {
 	*onec_str = (onec_string*)malloc(sizeof(onec_string));
 	(*onec_str)->val = (char*)malloc(4 * sizeof(char));
@@ -18,8 +15,8 @@ void _onec_string_init( onec_string** onec_str )
 	(*onec_str)->len = 0;
 }
 
-#define onec_string_put( onec_str, ch_val ) _onec_string_put(&onec_str, ch_val)
-void _onec_string_put( onec_string** onec_str, char ch_val )
+void inline __attribute__ ((always_inline))
+_onec_string_put( onec_string** onec_str, char ch_val )
 {
 	if( (*onec_str)->len >= (*onec_str)->size )
 	{
@@ -39,7 +36,6 @@ void _onec_string_put( onec_string** onec_str, char ch_val )
 	(*onec_str)->len += 1;
 }
 
-#define onec_string_append( onec_str, size, ... ) _onec_string_append(&onec_str, size, __VA_ARGS__)
 void _onec_string_append( onec_string** onec_str, size_t size, ... )
 {
 	va_list args;
@@ -59,8 +55,8 @@ void _onec_string_append( onec_string** onec_str, size_t size, ... )
 	va_end(args);
 }
 
-#define onec_string_insert( onec_str, string ) _onec_string_insert(&onec_str, string)
-void _onec_string_insert( onec_string** onec_str, char* string )
+void inline __attribute__ ((always_inline))
+_onec_string_insert( onec_string** onec_str, char* const string )
 {
 	char c;
 	size_t index = 0;
@@ -72,7 +68,6 @@ void _onec_string_insert( onec_string** onec_str, char* string )
 	}
 }
 
-#define onec_string_get( onec_str ) _onec_string_get(&onec_str)
 char* _onec_string_get( onec_string** onec_str )
 {
 	char *_onec_str = (char*)malloc((1 + (*onec_str)->len) * sizeof(char));
@@ -82,20 +77,87 @@ char* _onec_string_get( onec_string** onec_str )
 	return _onec_str;
 }
 
-#define onec_string_reset( onec_str ) _onec_string_reset(&onec_str)
-void _onec_string_reset( onec_string** onec_str )
+void inline __attribute__ ((always_inline))
+_onec_string_reset( onec_string** onec_str )
 {
 	memset((*onec_str)->val, 4, sizeof(char));
 	(*onec_str)->size = 4;
 	(*onec_str)->len = 0;
 }
 
-#define onec_string_release( onec_str ) _onec_string_release(&onec_str)
-void _onec_string_release( onec_string** onec_str )
+void inline __attribute__ ((always_inline))
+_onec_string_release( onec_string** onec_str )
 {	
 	if( (*onec_str)->val )
 		free((*onec_str)->val);
 	
 	if( (*onec_str) )
 		free(*onec_str);
+}
+
+void inline __attribute ((always_inline))
+_onec_string_initlc( onec_stringlc* onec_str )
+{
+	onec_str->val[0] = '\0';
+	onec_str->len = 0;
+}
+
+void inline __attribute__ ((always_inline))
+_onec_string_putlc( onec_stringlc* onec_str, char ch_val )
+{
+	onec_str->val[onec_str->len] = ch_val;
+	onec_str->len += 1;
+}
+
+void _onec_string_appendlc( onec_stringlc* onec_str, size_t size, ... )
+{
+	va_list args;
+	va_start(args, size);
+	for( size_t i = 0; i < size; i += 1 )
+	{
+		char* const string = va_arg(args, char* const);
+		char c;
+		size_t index = 0;
+		
+		while( (c = string[index]) != '\0' )
+		{
+			_onec_string_putlc(onec_str, c);
+			index += 1;
+		}
+	}
+	va_end(args);
+}
+
+char* _onec_string_getlc( onec_stringlc* onec_str )
+{
+	char* _onec_str = (char*)malloc((1 + onec_str->len) * sizeof(char));
+	strncpy(_onec_str, onec_str->val, onec_str->len);
+	_onec_str[onec_str->len] = '\0';
+	return _onec_str;
+}
+
+void inline __attribute__ ((always_inline))
+_onec_string_trimlc( onec_stringlc* onec_str )
+{
+	onec_str->val[onec_str->len] = '\0';
+}
+
+void inline __attribute__ ((always_inline))
+_onec_string_insertlc( onec_stringlc* onec_str, char* const string )
+{
+	char c;
+	size_t index = 0;
+	
+	while( (c = string[index]) != '\0' )
+	{
+		_onec_string_putlc(onec_str, c);
+		index += 1;
+	}
+}
+
+void inline __attribute__ ((always_inline))
+_onec_string_resetlc( onec_stringlc* onec_str )
+{
+	onec_str->val[0] = '\0';
+	onec_str->len = 0;
 }
--- include/onecstr.h
+++ include/onecstr.h
@@ -4,12 +4,20 @@
 
 #include <stdlib.h>
 
-typedef struct
+#define ONEC_STRING_INIT_LENGTH 1024
+
+typedef struct _onec_string
 {
 	char*     val;
 	size_t    size;
 	size_t    len;
 } onec_string;
+
+typedef struct _onec_stringlc
+{
+	char      val[ONEC_STRING_INIT_LENGTH];
+	size_t    len;
+} onec_stringlc;
 
 #define onec_string_init( onec_str ) _onec_string_init(&onec_str)
 void _onec_string_init( onec_string** onec_str );
@@ -21,7 +29,7 @@ void _onec_string_put( onec_string** onec_str, char ch_val );
 void _onec_string_append( onec_string** onec_str, size_t size, ... );
 
 #define onec_string_insert( onec_str, string ) _onec_string_insert(&onec_str, string)
-void _onec_string_insert( onec_string** onec_str, char* string );
+void _onec_string_insert( onec_string** onec_str, char* const string );
 
 #define onec_string_get( onec_str ) _onec_string_get(&onec_str)
 char* _onec_string_get( onec_string** onec_str );
@@ -31,5 +39,26 @@ void _onec_string_reset( onec_string** onec_str );
 
 #define onec_string_release( onec_str ) _onec_string_release(&onec_str)
 void _onec_string_release( onec_string** onec_str );
+
+#define onec_string_initlc( onec_str ) _onec_string_initlc(&onec_str)
+void _onec_string_initlc( onec_stringlc* onec_str );
+
+#define onec_string_putlc( onec_str, ch_val ) _onec_string_putlc(&onec_str, ch_val)
+void _onec_string_putlc( onec_stringlc* onec_str, char ch_val );
+
+#define onec_string_appendlc( onec_str, size, ... ) _onec_string_appendlc(&onec_str, size, __VA_ARGS__)
+void _onec_string_appendlc( onec_stringlc* onec_str, size_t size, ... );
+
+#define onec_string_getlc( onec_str ) _onec_string_getlc(&onec_str)
+char* _onec_string_getlc( onec_stringlc* onec_str );
+
+#define onec_string_trimlc( onec_str ) _onec_string_trimlc(&onec_str)
+void _onec_string_trimlc( onec_stringlc* onec_str );
+
+#define onec_string_insertlc( onec_str, string ) _onec_string_insertlc(&onec_str, string)
+void _onec_string_insertlc( onec_stringlc* onec_str, char* const string );
+
+#define onec_string_resetlc( onec_str ) _onec_string_resetlc(&onec_str)
+void _onec_string_resetlc( onec_stringlc* onec_str );
 
 #endif
