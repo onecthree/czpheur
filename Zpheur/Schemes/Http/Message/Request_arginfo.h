6af8a063f02acf6126526d9f612a79cd2de5f6c8
Sultan Ilham <onecthree@gmail.com>
2025-01-14 13:02:57 +0700

    Implements setter properties params for \Message\Request

--- Zpheur/Schemes/Http/Message/Request_arginfo.h
+++ Zpheur/Schemes/Http/Message/Request_arginfo.h
@@ -12,13 +12,26 @@
 
 zend_class_entry* zpheur_schemes_http_message_request_class_entry;
 
-ZEND_BEGIN_ARG_INFO_EX(Request___construct_arginfo, 0, 0, 6)
-    ZEND_ARG_TYPE_INFO(0, request, IS_ARRAY, 0)
-    ZEND_ARG_TYPE_INFO(0, query, IS_ARRAY, 0)
-    ZEND_ARG_TYPE_INFO(0, cookies, IS_ARRAY, 0)
-    ZEND_ARG_TYPE_INFO(0, attributes, IS_ARRAY, 0)
-    ZEND_ARG_TYPE_INFO(0, files, IS_ARRAY, 0)
-    ZEND_ARG_TYPE_INFO(0, server, IS_ARRAY, 0)
+ZEND_BEGIN_ARG_INFO_EX(Request___construct_arginfo, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Request_setBodyParams_arginfo, 0, 3, IS_VOID, 0)
+    ZEND_ARG_TYPE_INFO(0, request, IS_ARRAY | IS_NULL, 0)
+    ZEND_ARG_TYPE_INFO(0, query, IS_ARRAY | IS_NULL, 0)
+    ZEND_ARG_TYPE_INFO(0, cookies, IS_ARRAY | IS_NULL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Request_setFilesParams_arginfo, 0, 1, IS_VOID, 0)
+    ZEND_ARG_TYPE_INFO(0, files, IS_ARRAY | IS_NULL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Request_setHeaderParams_arginfo, 0, 2, IS_VOID, 0)
+    ZEND_ARG_TYPE_INFO(0, server, IS_ARRAY | IS_NULL, 0)
+    ZEND_ARG_TYPE_INFO(0, header, IS_ARRAY | IS_NULL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Request_setAttributeParams_arginfo, 0, 1, IS_VOID, 0)
+    ZEND_ARG_TYPE_INFO(0, attribute, IS_ARRAY | IS_NULL, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Request___get_arginfo, 0, 1, IS_MIXED, 0)
@@ -39,6 +52,10 @@ ZEND_END_ARG_INFO()
 
 
 PHP_METHOD(Request, __construct);
+PHP_METHOD(Request, setBodyParams);
+PHP_METHOD(Request, setFilesParams);
+PHP_METHOD(Request, setHeaderParams);
+PHP_METHOD(Request, setAttributeParams);
 PHP_METHOD(Request, __get);
 PHP_METHOD(Request, parseHeader);
 PHP_METHOD(Request, parseQuery);
@@ -47,6 +64,10 @@ PHP_METHOD(Request, __service);
 
 static const zend_function_entry zpheur_schemes_http_foundation_request_class_method[] = {
     PHP_ME(Request, __construct, Request___construct_arginfo, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+    PHP_ME(Request, setBodyParams, Request_setBodyParams_arginfo, ZEND_ACC_PUBLIC )
+    PHP_ME(Request, setFilesParams, Request_setFilesParams_arginfo, ZEND_ACC_PUBLIC )
+    PHP_ME(Request, setHeaderParams, Request_setHeaderParams_arginfo, ZEND_ACC_PUBLIC )
+    PHP_ME(Request, setAttributeParams, Request_setAttributeParams_arginfo, ZEND_ACC_PUBLIC )
     PHP_ME(Request, __get, Request___get_arginfo, ZEND_ACC_PUBLIC )
     PHP_ME(Request, parseHeader, Request_parseHeader_arginfo, ZEND_ACC_PUBLIC )
     PHP_ME(Request, parseQuery, Request_parseQuery_arginfo, ZEND_ACC_PUBLIC )
