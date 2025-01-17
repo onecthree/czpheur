652173ff5181c0189ec2330f54355bfdf9b0d8e2
Sultan Ilham <onecthree@gmail.com>
2025-01-17 14:12:05 +0700

    Remove exitCode()

--- Zpheur/Actions/Console/DefaultAction_arginfo.h
+++ Zpheur/Actions/Console/DefaultAction_arginfo.h
@@ -15,16 +15,12 @@ zend_class_entry* zpheur_actions_console_defaultaction_class_entry;
 ZEND_BEGIN_ARG_INFO_EX(Console_DefaultAction___construct_arginfo, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Console_DefaultAction_exitCode_arginfo, 0, 1, IS_LONG, 0)
-    ZEND_ARG_TYPE_INFO(0, code, IS_LONG, 0)
-ZEND_END_ARG_INFO()
 
 PHP_METHOD(Console_DefaultAction, __construct);
-PHP_METHOD(Console_DefaultAction, exitCode);
+
 
 static const zend_function_entry zpheur_actions_console_defaultaction_class_method[] = {
 	PHP_ME(Console_DefaultAction, __construct, Console_DefaultAction___construct_arginfo, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-	PHP_ME(Console_DefaultAction, exitCode, Console_DefaultAction_exitCode_arginfo, ZEND_ACC_PUBLIC)
 	ZEND_FE_END
 };
 
--- Zpheur/Actions/Console/DefaultAction_class.c
+++ Zpheur/Actions/Console/DefaultAction_class.c
@@ -55,25 +55,6 @@ PHP_METHOD(Console_DefaultAction, __construct)
     ZEND_PARSE_PARAMETERS_NONE();
 }
 
-PHP_METHOD(Console_DefaultAction, exitCode)
-{
-    zend_long exit_code = 0;
-
-    ZEND_PARSE_PARAMETERS_START(1, 1)
-        Z_PARAM_LONG(exit_code)
-    ZEND_PARSE_PARAMETERS_END();
-
-    console_default_action_object* instance = 
-        ZPHEUR_ZVAL_GET_OBJECT(console_default_action_object, getThis());
-
-    instance->common->exit_code = exit_code;
-    
-    zend_register_long_constant("EXIT_CODE", sizeof("EXIT_CODE") -1, 
-        exit_code, CONST_CS | CONST_PERSISTENT, 51);
-
-    RETURN_LONG(exit_code);
-}
-
 ZEND_MINIT_FUNCTION(Zpheur_Actions_Console_DefaultAction)
 {
     zend_class_entry ce;
