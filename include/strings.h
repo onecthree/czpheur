87749b06aa87b2482ad452e4608b192d42a48d19
Sultan Ilham <onecthree@gmail.com>
2025-01-17 12:33:49 +0700

    Rename module name

---
+++ include/strings.h
@@ -0,0 +1,10 @@
+
+#ifndef H_INCLUDE_LIBSTR
+#define H_INCLUDE_LIBSTR
+
+#include <stdlib.h>
+
+// char* md5_file_sum( char const* filename );
+unsigned long string_to_digits( char* source_src, size_t source_len, size_t limit );
+
+#endif
